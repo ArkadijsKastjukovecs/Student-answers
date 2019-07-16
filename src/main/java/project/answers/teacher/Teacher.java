882b45b52ab227cb8dfaaa6b8f895119c33b4963
Martin Ose <martinose9@gmail.com>
2019-07-16 12:09:42 +0300

    Small success!

--- src/main/java/project/answers/teacher/Teacher.java
+++ src/main/java/project/answers/teacher/Teacher.java
@@ -1,7 +1,34 @@
 package project.answers.teacher;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 // Teacher webpage 
 
+@RestController
+@RequestMapping(value = "/", produces = "text/html;charset=UTF-8")
 public class Teacher {
+	
+	@GetMapping("")
+	@ResponseBody
+	
+	public String homePage(@RequestParam(value = "file", required = false) String name, HttpServletRequest request,
+			HttpServletResponse response){
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("<p> <a href='/SelectExcel'>SelectExcel</a> <button type='button'>Upload</button> </p>");
+		
+		return sb.toString();
+		
+		
+	}
 
 }
