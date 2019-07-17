67c72234a126bed8a7a304b38bd5b6132a42bc44
Pavels Belovs <pavelbelov88@gmail.com>
2019-07-17 14:29:19 +0300

    check

--- src/main/java/project/answers/student/StudentController.java
+++ src/main/java/project/answers/student/StudentController.java
@@ -12,7 +12,7 @@ import project.answers.tests.Test;
 public class StudentController {
 	protected Connection conn;
 	public StudentController(){
-		//gjkg
+		//gjk
 	/*	conn = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
