ac1f5aa5e81cd3f2b9aefe350fe76bfc403e31f0
Arkadijs Kastjukovecs <arkadij777@inbox.lv>
2019-07-23 10:25:08 +0300

    newest version

--- src/main/java/project/answers/server/Addtest.java
+++ src/main/java/project/answers/server/Addtest.java
@@ -2,13 +2,17 @@ package project.answers.server;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import project.answers.tests.Test;
 
@@ -21,10 +25,21 @@ public class Addtest {
 	}
 
 	@RequestMapping(value = "/addTest", method = RequestMethod.POST)
-	public String setTest(@ModelAttribute(name = "addTest") Test test, Model model) throws IOException {
+	public String setTest(@ModelAttribute(name = "addTest") Test test, @RequestParam("file") MultipartFile file,
+			Model model) throws IOException {
 		System.out.println(test);
-		File copied = new File("/test/"+test.getFile().getName());
-		 FileUtils.copyFile(test.getFile(), copied);
+		File copied = new File("/test/" + test.getFile().getName());
+		System.out.println("file name	: " + file.getName());
+		try {
+
+			// Get the file and save it somewhere
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get("/home/student/workspace/newProj/answers/tests/" + file.getOriginalFilename());
+			Files.write(path, bytes);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return "index";
 	}
 }
