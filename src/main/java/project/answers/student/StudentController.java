b5da5e619c160e4d0ca945e196de499d6a113707
Arkadijs Kastjukovecs <arkadij777@inbox.lv>
2019-07-24 17:16:04 +0300

    StudentController commit before merge

--- src/main/java/project/answers/student/StudentController.java
+++ src/main/java/project/answers/student/StudentController.java
@@ -21,134 +21,130 @@ import project.answers.tests.Test;
 public class StudentController {
 	private static Test test;
 	private static File file;
-//	private boolean downloadedTest = true;
-//	protected Connection conn;
-	/*conn = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://192.168.8.124:3305/?autoReconnect=true&useSSL=false&characterEncoding=utf8", "student",
-					"Student007");
-			//conn.setAutoCommit(false);
-			System.out.println("Is connection");
-			// conn = DriverManager.getConnection(
-			// "database_activity","root", "Student007");
-			// System.out.println("Is connection");
-		} catch (SQLException | ClassNotFoundException e) {
-			System.out.println(e);
-		}
-		
-	}
-	
-	public Student getFullRowExcel(int rowWithFile)throws SQLException{
-		String query = "select * from database_Tests.Tests1 where id = (?)";
-		PreparedStatement ps = conn.prepareStatement(query);
-		ps.setInt(1, rowWithFile);
-		ResultSet rs = ps.executeQuery();
-		Student student = new Student();
-		while(rs.next()){
-		//	for(int i = 0; i<2; i++){
-				student.setTrueAnswers(rs.getString("answer1"), 0);
-				student.setTrueAnswers(rs.getString("answer2"), 1);
-		//	}
-		}
-		return student;
-	}
-	
-	
-	public int submitAnswers(Student student, String answer1, String answer2)throws SQLException{
-		if(student.getTrueAnswers(0) == answer1){
-			student.setScore(student.getScore()+1);
-		}
-		if(student.getTrueAnswers(1) == answer2){
-			student.setScore(student.getScore()+1);
-		}
-		return student.getScore();
-	}
-	public Student setName(Student student, String name){
-		student.setStudName(name);
-		return student;
-	}*/
+
+	// private boolean downloadedTest = true;
+	// protected Connection conn;
+	/*
+	 * conn = null; try { Class.forName("com.mysql.jdbc.Driver"); conn =
+	 * DriverManager.getConnection(
+	 * "jdbc:mysql://192.168.8.124:3305/?autoReconnect=true&useSSL=false&characterEncoding=utf8",
+	 * "student", "Student007"); //conn.setAutoCommit(false);
+	 * System.out.println("Is connection"); // conn =
+	 * DriverManager.getConnection( // "database_activity","root",
+	 * "Student007"); // System.out.println("Is connection"); } catch
+	 * (SQLException | ClassNotFoundException e) { System.out.println(e); }
+	 * 
+	 * }
+	 * 
+	 * public Student getFullRowExcel(int rowWithFile)throws SQLException{
+	 * String query = "select * from database_Tests.Tests1 where id = (?)";
+	 * PreparedStatement ps = conn.prepareStatement(query); ps.setInt(1,
+	 * rowWithFile); ResultSet rs = ps.executeQuery(); Student student = new
+	 * Student(); while(rs.next()){ // for(int i = 0; i<2; i++){
+	 * student.setTrueAnswers(rs.getString("answer1"), 0);
+	 * student.setTrueAnswers(rs.getString("answer2"), 1); // } } return
+	 * student; }
+	 * 
+	 * 
+	 * public int submitAnswers(Student student, String answer1, String
+	 * answer2)throws SQLException{ if(student.getTrueAnswers(0) == answer1){
+	 * student.setScore(student.getScore()+1); } if(student.getTrueAnswers(1) ==
+	 * answer2){ student.setScore(student.getScore()+1); } return
+	 * student.getScore(); } public Student setName(Student student, String
+	 * name){ student.setStudName(name); return student; }
+	 */
 	@RequestMapping(value = "/student", produces = "text/html;charset=UTF-8", method = RequestMethod.GET)
-	public String helloWorld(HttpServletRequest request, HttpServletResponse response, Model model){
-		
-	//	Server.testController.SetActiveTest(test);
-	//	System.out.println(Server.testController.getActiveTest());
+	public String helloWorld(HttpServletRequest request, HttpServletResponse response, Model model) {
+
+		// Server.testController.SetActiveTest(test);
+		// System.out.println(Server.testController.getActiveTest());
 		model.addAttribute("isActive", Server.testController.getActiveTest());
 		return "StudentView";
 	}
 
 	@RequestMapping(value = "/student/excelFile", method = RequestMethod.POST)
-	public void excelFile(HttpServletRequest request, HttpServletResponse response)throws IOException{
+	public void excelFile(HttpServletRequest request, HttpServletResponse response) {
 		test = Server.testController.getTest(request.getParameter("testName"));
-		if(StudentController.activeTest()){
-			 file = Server.testController.getTest(request.getParameter("testName")).getFile();
-		}else{
+		if (StudentController.activeTest()) {
+			file = Server.testController.getTest(request.getParameter("testName")).getFile();
+		} else {
 			file = Server.testController.getActiveTest().getFile();
 		}
-		OutputStream out = response.getOutputStream();
-		FileInputStream in = new FileInputStream(file.getAbsolutePath());
-		byte[] buffer = new byte[4096];
-		int length;
-		while ((length = in.read(buffer)) > -1){
-		    out.write(buffer, 0, length);
+		System.out.println("TEST: "+test+", FILE: " + file);
+		if (file != null) {
+			try {
+				
+			OutputStream out = response.getOutputStream();
+			FileInputStream in = new FileInputStream(file.getAbsolutePath());
+			byte[] buffer = new byte[4096];
+			int length;
+			while ((length = in.read(buffer)) > -1) {
+				out.write(buffer, 0, length);
+			}
+			System.out.println(file.getName());
+
+			in.close();
+			out.flush();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		System.out.println(file.getName());
-		
-		in.close();
-		out.flush();
 
 		System.out.println(request.getParameter("testName"));
 	}
-	
+
 	@RequestMapping(value = "/student/buttonSubmit", method = RequestMethod.POST)
 	public String buttonSubmit(HttpServletRequest request, Model model) {
-//		System.out.println(request.getParameter("answer1")+" "+ request.getParameter("answer2"));
+		// System.out.println(request.getParameter("answer1")+" "+
+		// request.getParameter("answer2"));
 		int localScore = 0;
 		if (request.getParameter("answer1") != "" && request.getParameter("answer2") != "") {
 			try {
-//				downloadedTest = true;
-				localScore = StudentController.getAnswers(request.getParameter("answer1"), 
-				request.getParameter("answer2"), request.getParameter("vards"));
+				// downloadedTest = true;
+				localScore = StudentController.getAnswers(request.getParameter("answer1"),
+						request.getParameter("answer2"), request.getParameter("vards"));
 				model.addAttribute("localScore", String.valueOf(localScore));
 			} catch (MultiStudentNameException e) {
 				model.addAttribute("localScore", "Sis students ar tadu vardu jau pildija so testu");
-//				return "StudentScore";
-				}
-//			catch(NullPointerException e){
-//					downloadedTest = false;
-//					model.addAttribute("downloadedTest", String.valueOf(downloadedTest));
-//					return "StudentScore";
-//				}
-//			model.addAttribute("downloadedTest", String.valueOf(downloadedTest));
+				// return "StudentScore";
+			}
+			// catch(NullPointerException e){
+			// downloadedTest = false;
+			// model.addAttribute("downloadedTest",
+			// String.valueOf(downloadedTest));
+			// return "StudentScore";
+			// }
+			// model.addAttribute("downloadedTest",
+			// String.valueOf(downloadedTest));
 			System.out.println(localScore);
 		}
-		
+
 		return "StudentScore";
 	}
-	
-	public static int getAnswers(String one, String two, String vards) throws MultiStudentNameException{
-		
+
+	public static int getAnswers(String one, String two, String vards) throws MultiStudentNameException {
+
 		int localScore = 0;
-		if(one.equals(test.getAnswer1())){
-			
+		if (one.equals(test.getAnswer1())) {
+
 			localScore++;
 		}
-		if(two.equals(test.getAnswer2())){
-			
+		if (two.equals(test.getAnswer2())) {
+
 			localScore++;
 		}
 		Student student = new Student(vards, file.getName(), localScore);
 		System.out.println(student.toString());
 		Server.testController.AddStudent(student);
 		return student.getScore();
-		
+
 	}
-	public static boolean activeTest(){
+
+	public static boolean activeTest() {
 		Test test = Server.testController.getActiveTest();
-		if(test==null){
-		return true;
-		}else{
+		if (test == null) {
+			return true;
+		} else {
 			return false;
 		}
 	}
